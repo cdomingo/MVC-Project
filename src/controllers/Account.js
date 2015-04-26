37205886a17a9ea23e7f4ecc52a0e527cfc16fb7
cdomingo <carl.m.domingo@gmail.com>
2015-04-26 19:30:34 -0400

    Fix Account.js

--- src/controllers/Account.js
+++ src/controllers/Account.js
@@ -61,8 +61,8 @@ var signup = function(req, res) {
 			}
 
             req.session.account = newAccount.toAPI();
-
-            res.render('app', {users: username});
+            
+			res.json({redirect: '/maker'});
 		});
 	});
 };
